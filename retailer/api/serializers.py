6c271373541ed9f5819d4109ec61eb5eb87231db
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-04-09 18:46:36 -0500

    Included FKs
    Foreign keys where not being added. Corrected that

--- retailer/api/serializers.py
+++ retailer/api/serializers.py
@@ -3,16 +3,22 @@ from rest_framework import serializers
 from .models import Orders, Payments, Shippings, Users
 
 class Orders_serializer(serializers.HyperlinkedModelSerializer):
+    # To incluide the FK from another table
+    user = serializers.ReadOnlyField(source='user.name')
     class Meta:
         model = Orders
         fields = ('order_id', 'date', 'total', 'subtotal', 'taxes', 'paid', 'user')
 
 class Payments_serializer(serializers.HyperlinkedModelSerializer):
+    # To incluide the FK from another table
+    order = serializers.ReadOnlyField(source='order.order_id')
     class Meta:
         model = Payments
         fields = ('payment_id', 'type', 'date', 'txn_id', 'total', 'status', 'order')
 
 class Shippings_serializer(serializers.HyperlinkedModelSerializer):
+    # To incluide the FK from another table
+    order = serializers.ReadOnlyField(source='order.order_id')
     class Meta:
         model = Shippings
         fields = ('ship_id', 'address', 'city', 'state', 'country', 'cost', 'order')
